0329aab25e4b44d8ee2669f87a3f2b25c8809b74
AyrielNoriega <ayriel.rentek@rentek.com.co>
2024-02-21 15:34:57 -0500

    133. Formik - Obtener información del formulario

--- 02-react-app/src/03-forms/pages/FormikBasicPage.tsx
+++ 02-react-app/src/03-forms/pages/FormikBasicPage.tsx
@@ -1,15 +1,32 @@
+import { useFormik } from "formik";
+
 import '../styles/styles.css';
 
 export const FormikBasicPage = () => {
+
+    const { handleChange, values, handleSubmit } = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+        },
+        onSubmit: (values) => {
+            console.log(values);
+            
+        }
+    });
+
     return (
         <div>
             <h1>FormikBasicPage</h1>
 
-            <form noValidate>
+            <form noValidate onSubmit={ handleSubmit }>
                 <label htmlFor="firstName">First Name</label>
                 <input
                     type="text"
                     name='firstName'
+                    onChange={ handleChange }
+                    value={ values.firstName }
                 />
                 <span>first name is required</span>
 
@@ -17,6 +34,8 @@ export const FormikBasicPage = () => {
                 <input
                     type="text"
                     name='lastName'
+                    onChange={ handleChange }
+                    value={ values.lastName }
                 />
                 <span>last name is required</span>
 
@@ -24,6 +43,8 @@ export const FormikBasicPage = () => {
                 <input
                     type="text"
                     name='email'
+                    onChange={ handleChange }
+                    value={ values.email }
                 />
                 <span>email is required</span>
 
